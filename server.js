e3074c643e05751617ad48ecdbc5aa0e0f4332b0
Lucas do Nascimento Vieira <vieira.lucas@virtualserver01.daae.mitraonline.com.br>
2020-11-13 11:22:58 -0300

    Configuracao servidor

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express')
 
-const webMapa = express()
 
-const base = '/home/prefeitura/front-web-mappa/build';
+//FRONT CARTA DE SERVICO
+const cartaServico = express()
 
-webMapa.use(express.static(`${base}`))
+const urlCartaServico = 'D:/web/safe/daae/front-sou-cidadao-carta-servico/build';
 
-webMapa.get('*', (req,res) => res.sendFile('index.html' , { root : base }))
+cartaServico.use(express.static(`${urlCartaServico}`))
+
+cartaServico.get('*', (req,res) => res.sendFile('index.html' , { root : urlCartaServico }))
 
 const port = 8000
 
-webMapa.listen(port, () => console.log('Mapa Web disponibilizado com sucesso.'))
+cartaServico.listen(port, () => console.log('Carta de servico disponibilizado com sucesso.'))
+
+
+//FRONT SOU CIDADAO ORGANIZACAO
+const organizacao = express()
+
+const urlOrganizacao = 'D:/web/safe/daae/front-sou-cidadao-organizacao/build';
+
+organizacao.use(express.static(`${urlOrganizacao}`))
+
+organizacao.get('*', (req,res) => res.sendFile('index.html' , { root : base }))
+
+const port = 8001
+
+organizacao.listen(port, () => console.log('Sou cidadao organizacao disponibilizado com sucesso.'))
